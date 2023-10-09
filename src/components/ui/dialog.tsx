bfa169ad880af7748d9a0667bdb9ebb508f78949
Abdelmonaem <abdelmonaem@me.com>
2023-10-09 23:24:43 +0300

    fix another issue

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -11,7 +11,7 @@ const DialogTrigger = DialogPrimitive.Trigger;
 const DialogPortal = ({
   className,
   ...props
-}: DialogPrimitive.DialogPortalProps & { className: string }) => (
+}: DialogPrimitive.DialogProps & { className: string }) => (
   <DialogPrimitive.Portal className={cn(className)} {...props} />
 );
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;
