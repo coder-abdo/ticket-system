6c21d2e8b9fe9ec06001b85baa7219faf5c3f5bf
Abdelmonaem <abdelmonaem@me.com>
2023-10-09 22:25:51 +0300

    fix build issues

--- src/hooks/useDropImages.ts
+++ src/hooks/useDropImages.ts
@@ -1,25 +1,5 @@
-import { useState, useCallback } from "react";
-import { DropEvent, FileRejection, useDropzone } from "react-dropzone";
+import { useDropzone } from "react-dropzone";
 export const useImageDrop = () => {
-  const [files, setFiles] = useState([]);
-  const onDrop = useCallback<
-    <T extends File>(
-      acceptedFiles: T[],
-      fileRejections: FileRejection[],
-      event: DropEvent,
-    ) => void | undefined
-  >((acceptedFiles) => {
-    if (acceptedFiles?.length) {
-      setFiles((prevFiles) => {
-        return [
-          ...prevFiles,
-          ...acceptedFiles.map((file) =>
-            Object.assign(file, { preview: URL.createObjectURL(file) }),
-          ),
-        ];
-      });
-    }
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-  return { getRootProps, getInputProps, isDragActive, files };
+  const { getRootProps, getInputProps, isDragActive } = useDropzone();
+  return { getRootProps, getInputProps, isDragActive };
 };
